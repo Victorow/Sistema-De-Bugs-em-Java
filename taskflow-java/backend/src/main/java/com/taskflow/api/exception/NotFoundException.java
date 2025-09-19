9e1540ef3d3ae015287257f7b68882122cf1725c
Victor Augusto Ferreira dos Santos <142556568+Victorow@users.noreply.github.com>
2025-09-19 01:06:55 -0300

    Delete taskflow-java directory
    separação das pastas em frontend e backend

--- taskflow-java/backend/src/main/java/com/taskflow/api/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-// backend/src/main/java/com/taskflow/api/exception/NotFoundException.java
-package com.taskflow.api.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
